8147b94356e4c4f5af97d9ac7381196ef3e5fc12
hectorruiz572 <hectorruiz572@gmail.com>
2024-12-16 18:54:02 +0100

    cambios router

--- src/app/Router.jsx
+++ src/app/Router.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "../pages/Home";
 import Perfil from "../pages/Perfil";
 import Layout from "../Layout/Layout";
+import PrivateRoute from "../components/PrivateRoute"; // Importando el componente de ruta privada
 import ErrorPage from "../pages/ErrorPage";
 import Comunidad from "../pages/Comunidad";
 import PlaceDetails from "../pages/PlaceDetails";
@@ -12,11 +13,22 @@ const Router = () => {
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
-          {/* Eliminar PrivateRoute: cualquier usuario puede acceder a la ruta de perfil */}
-          <Route path="/perfil" element={<Perfil />} />
+
+          {/* Ruta privada, que solo es accesible si el usuario está logueado */}
+          <Route
+            path="/perfil"
+            element={
+              <PrivateRoute>
+                <Perfil />
+              </PrivateRoute>
+            }
+          />
+
           <Route path="/:comunidad" element={<Comunidad />} />
           <Route path="/:comunidad/:placeName" element={<PlaceDetails />} />
-          <Route path="/error" element={<ErrorPage />} /> {/* Ruta de error */}
+
+          {/* Ruta de error, si el usuario no está autenticado o hay un error */}
+          <Route path="/error" element={<ErrorPage />} />
         </Route>
       </Routes>
     </BrowserRouter>
