401024ea7e79c58b9350440f8090c72c14ee44fe
hectorruiz572 <hectorruiz572@gmail.com>
2024-12-16 19:06:53 +0100

    cambios router

--- src/components/PrivateRoute.jsx
+++ src/components/PrivateRoute.jsx
@@ -3,13 +3,16 @@ import { Navigate } from "react-router-dom";
 
 // PrivateRoute para manejar rutas protegidas
 const PrivateRoute = ({ children }) => {
+  // Verifica si el usuario está autenticado usando localStorage
   const isLoggedIn = localStorage.getItem("isLoggedIn") === "true"; // Verifica si el usuario está logueado
 
   if (!isLoggedIn) {
-    return <Navigate to="/error" />; // Si no está logueado, redirige a la página de error
+    // Si no está logueado, redirige a la página de error
+    return <Navigate to="/error" />;
   }
 
-  return children; // Si está logueado, renderiza la ruta protegida
+  // Si está logueado, renderiza la ruta protegida
+  return children;
 };
 
 export default PrivateRoute;
