adce59fe793d3bf0df7d6e0e00283bbb7e48a1d4
hectorruiz572 <hectorruiz572@gmail.com>
2024-12-16 19:06:16 +0100

    cambios router

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/', // Si la aplicación está en la raíz
+  // Si tu aplicación está en una subcarpeta, usa algo como:
+  // base: '/mi-subcarpeta/'
 })
