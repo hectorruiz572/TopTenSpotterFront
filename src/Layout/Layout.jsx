77410cc3f976d3e2f732b085a7548beef4ad12dd
hectorruiz572 <hectorruiz572@gmail.com>
2024-12-17 15:38:09 +0100

    cambios imagen perfil

--- src/Layout/Layout.jsx
+++ src/Layout/Layout.jsx
@@ -105,7 +105,7 @@ const Layout = () => {
                   <img
                     src={
                       userData.photo
-                        ? `https://toptenspotterbackend.onrender.com/profileimg/${userData.photo}`
+                        ? `https://toptenspotterbackend.onrender.com/uploads/profileimg/${userData.photo}`
                         : iconoPerfil
                     }
                     alt="Perfil"
